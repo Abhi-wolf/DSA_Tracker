ed205409f577fd04a8cd5517b8853e95a87f6cc8
Abhi-wolf <abhijeetkr2020@outlook.com>
2024-08-29 18:46:32 +0530

    Added Notes Editor

--- app/tag/[tagName]/[topicName]/error.jsx
+++ app/tag/[tagName]/[topicName]/error.jsx
@@ -3,13 +3,12 @@
 import { Button } from "@/components/ui/button";
 
 export default function Error({ error, reset }) {
-  console.log(error);
   return (
     <main className="flex min-h-[80vh] md:min-h-[50vh] justify-center items-center flex-col gap-6">
       <h1 className="text-3xl font-semibold text-red-500">
         Something went wrong!
       </h1>
-      <p className="text-lg text-red-400">{error.message}</p>
+      {/* <p className="text-lg text-red-400">{error.message}</p> */}
 
       <Button onClick={reset}>Try again</Button>
     </main>
